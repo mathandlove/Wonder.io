71087d7828d02e5b96e25ec51f628f45d16fe027
mathandlove <53538789+mathandlove@users.noreply.github.com>
2021-06-16 19:06:57 -0600

    StartingToMakeReadElementsWork

---
+++ src/components/booklayout/ReadElements.vue
@@ -0,0 +1,25 @@
+ <template>
+  <div class="mainText">
+    {{ textSeries + textSeries }}
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  components: {},
+  methods: {
+    setupNIV() {},
+  },
+  mounted() {},
+  dismounted() {},
+  computed: {
+    ...mapGetters(["textSeries"]),
+  },
+};
+</script>
+
+<style scoped>
+.mainText {
+  white-space: pre-line;
+}
+</style>
